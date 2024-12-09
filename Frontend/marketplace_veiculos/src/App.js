ddd619e79c51d13d7059dabd3660124531b55874
ellen samanta <ellen.samanta@outlook.com>
2024-12-09 20:24:22 -0300

    chore: chamando o dashboard na pagina principal

--- Frontend/marketplace_veiculos/src/App.js
+++ Frontend/marketplace_veiculos/src/App.js
@@ -2,11 +2,15 @@
 
 import React from'react';
 import Cabecalho from './paginas/cabecalho';
+import Dashboard from './paginas/dashboard';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Cabecalho/>
+        <>
+          <Cabecalho/>
+          <Dashboard/>
+        </>
       </header>
     </div>
   );
